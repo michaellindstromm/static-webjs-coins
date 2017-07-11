0b1d210ddf17f63f09d3d71881b0e9fff13fbe3f
Michael Lindstrom <michaellindstr@gmail.com>
2017-07-11 17:27:26 -0500

    set up problem. No action taken

---
+++ coins.js
@@ -0,0 +1,26 @@
+/*
+  Input: 0.67
+  Output:
+  {
+    quarters: 2,
+    dimes: 1,
+    nickels: 1,
+    pennies: 2
+  }
+
+*/
+
+function coinCounter () {
+  // Initialize a JavaScript object to hold the coins
+  var coinPurse = {};
+
+  coinPurse.quarters = 0;
+  coinPurse.dimes = 0;
+  coinPurse.nickels = 0;
+  coinPurse.pennies = 0;
+
+  return coinPurse;
+}
+
+var coins = coinCounter()
+console.log();
