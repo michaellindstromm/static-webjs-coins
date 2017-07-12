32e348b92ccd030032c2fbc72a440f766b415518
Michael Lindstrom <michaellindstr@gmail.com>
2017-07-11 22:43:05 -0500

    converted to string. split string. converted to number. working with number and trying to figure out how to do it. were getting somewhere.

--- coins.js
+++ coins.js
@@ -10,17 +10,32 @@
 
 */
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
+// function coinCounter (amount) {
+// Initialize a JavaScript object to hold the coins
+var amount = 16.83;
+var string = amount.toString();
+var nums = string.split(".");
 
-  coinPurse.quarters = 0;
-  coinPurse.dimes = 0;
-  coinPurse.nickels = 0;
-  coinPurse.pennies = 0;
+var coinPurse = {};
 
-  return coinPurse;
+coinPurse.quarters = 0;
+coinPurse.dimes = 0;
+coinPurse.nickels = 0;
+coinPurse.pennies = 0;
+
+for (var i = 0; i < nums.length; i++) {
+  nums[i] = Number(nums[i]);
 }
 
-var coins = coinCounter()
-console.log();
+coinPurse.quarters = nums[0]*4;
+if (nums[1] % 25 === 0) {
+
+}
+
+
+//   return coinPurse;
+// }
+//
+// var coins = coinCounter()
+console.log(nums);
+console.log(coinPurse.quarters);
